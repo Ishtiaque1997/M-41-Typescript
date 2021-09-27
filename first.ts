24ecb93cee497bb38b02df1353ffe8cfe37a22ae
Ishtiaque1997 <ishtiaquea695@gmail.com>
2021-09-27 12:34:00 +0600

    more to go

--- first.ts
+++ first.ts
@@ -5,3 +5,30 @@
 let money:number=90;
 let str:string='hello';
 let bool:boolean=true;
+console.log(bool)
+
+
+
+function add(first:number,second:number):number{
+ const res=first+second;
+ return res;
+}
+
+const output:number=add(23,25)
+console.log(output)
+
+// function add(first:number | string,second:number | string):number |string{
+//  const res:string|number=first+second;
+//  return res;
+// }
+
+// const output:number=add(23,25)
+
+// const output1=add('sakib','khan')
+
+
+
+
+
+
+
--- js/first.js
+++ js/first.js
@@ -5,3 +5,16 @@
 let money = 90;
 let str = 'hello';
 let bool = true;
+console.log(bool);
+function add(first, second) {
+    const res = first + second;
+    return res;
+}
+const output = add(23, 25);
+console.log(output);
+// function add(first:number | string,second:number | string):number |string{
+//  const res:string|number=first+second;
+//  return res;
+// }
+// const output:number=add(23,25)
+// const output1=add('sakib','khan')
